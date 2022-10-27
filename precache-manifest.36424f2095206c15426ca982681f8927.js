b99c9bb325bd61d3b0a82a8a2b6123d6e63fdba8
kiran7117 <102372038+kiran7117@users.noreply.github.com>
2022-10-28 02:23:50 +0530

    Updates

---
+++ precache-manifest.36424f2095206c15426ca982681f8927.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8b93e969959e5a5b0cc625471870c8",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "91a8ce9d867e3d9aab4d",
+    "url": "/keeper/static/js/2.7358e7b1.chunk.js"
+  },
+  {
+    "revision": "f59b8bbdd05d8d2117ff",
+    "url": "/keeper/static/js/main.09b4f8fc.chunk.js"
+  },
+  {
+    "revision": "ee38aac5cc265be45431",
+    "url": "/keeper/static/js/runtime-main.d35bd170.js"
+  }
+]);
